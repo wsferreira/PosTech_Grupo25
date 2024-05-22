d0c288c8e0ac9a6668703b8df7ab1a1929b3a006
wsferreira <wferreira.contato@gmail.com>
2024-05-21 22:34:22 -0300

    ordenação testes

--- Fase1/PosTech.Grupo25.Fase1/PosTech.Contatos.API.Tests/Repository/ContatoRepositoryTests.cs
+++ Fase1/PosTech.Grupo25.Fase1/PosTech.Contatos.API.Tests/Repository/ContatoRepositoryTests.cs
@@ -23,13 +23,14 @@ namespace PosTech.Contatos.API.Tests.Repository
             //Configuração do EF InMemory
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseInMemoryDatabase(databaseName: "teste_postech_fase1")
-            .Options;
+            .EnableSensitiveDataLogging()
+            .Options;            
 
             _context = new ApplicationDbContext(options);
             _contatoRepository = new ContatoRepository(_context);
         }
 
-        [Fact(DisplayName = "Validando se a lista de contatos é retornada com sucesso")]
+        [Fact(DisplayName = "1. Validando se a lista de contatos é retornada com sucesso")]
         [Trait("Categoria", "Validando Contato")]
         public void ObterTodos_DeveRetornarListaDeContatos()
         {
@@ -52,7 +53,7 @@ namespace PosTech.Contatos.API.Tests.Repository
             Assert.NotNull(contatos);
         }
 
-        [Fact(DisplayName = "Validando se a lista de contatos por região é retornada com apenas mesmo DDD")]
+        [Fact(DisplayName = "2. Validando se a lista de contatos por região é retornada com apenas mesmo DDD")]
         [Trait("Categoria", "Validando Contato")]
         public void ObterContatosPorRegiao_DeveRetornarListaDeContatosMesmoDDD()
         {
@@ -69,7 +70,7 @@ namespace PosTech.Contatos.API.Tests.Repository
         }
 
 
-        [Fact(DisplayName = "Validando o contato por Id retorna o mesmo registro")]
+        [Fact(DisplayName = "3. Validando o contato por Id retorna o mesmo registro")]
         [Trait("Categoria", "Validando Contato")]
         public void ObterPorId_DeveRetornarContatoExistente()
         {
@@ -93,7 +94,7 @@ namespace PosTech.Contatos.API.Tests.Repository
             Assert.Equal(contatoEsperado.Nome, contato.Nome);
         }
 
-        [Fact(DisplayName = "Validando o contato por Id retorna nenhum registro")]
+        [Fact(DisplayName = "4. Validando o contato por Id retorna nenhum registro")]
         [Trait("Categoria", "Validando Contato")]
         public void ObterPorId_DeveRetornarNull()
         {
@@ -109,7 +110,7 @@ namespace PosTech.Contatos.API.Tests.Repository
         }
 
 
-        [Fact(DisplayName = "Validando se o cadastro não retorna exceções")]
+        [Fact(DisplayName = "5. Validando se o cadastro não retorna exceções")]
         [Trait("Categoria", "Validando Contato")]
         public void Cadastrar_DeveExecutarSemExcecoes()
         {
@@ -122,16 +123,14 @@ namespace PosTech.Contatos.API.Tests.Repository
         }
 
 
-        [Fact(DisplayName = "Validando se a alteração não retorna exceções")]
+        [Fact(DisplayName = "6. Validando se a alteração não retorna exceções")]
         [Trait("Categoria", "Validando Contato")]
         public void Alterar_DeveExecutarSemExcecoes()
         {
 
-            // Arrange
-            /*_context.Contato.Add(new Contato { Id = 7, Nome = "Denise", Email = "denise@email.com", Telefone = "1234-5678", RegiaoId = 11 });
-            _context.SaveChanges();
-           
-            var contatoParaAlterar = _contatoRepository.ObterPorId(7);
+            // Arrange         
+            /*var contatoParaAlterar = new Contato { Id = 1, Nome = "Denise", Email = "denise@email.com", Telefone = "1234-5678", RegiaoId = 11 };
+                
             contatoParaAlterar.Nome = "Denise 2";           
 
             // Act & Assert
@@ -147,7 +146,7 @@ namespace PosTech.Contatos.API.Tests.Repository
             Assert.Null(exception);
         }
 
-        [Fact(DisplayName = "Validando se a exclusão não retorna exceções")]
+        [Fact(DisplayName = "7. Validando se a exclusão não retorna exceções")]
         [Trait("Categoria", "Validando Contato")]
         public void Deletar_DeveExecutarSemExcecoes()
         {
