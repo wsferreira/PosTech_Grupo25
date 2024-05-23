a060bb8cb5a8e199d990f1169b1d59749a71f6b5
Ian <mello.ian@hoymail.com>
2024-05-23 20:15:32 -0300

    add Views/Home/AdicionaContato.cshtml

---
+++ Fase1/PosTech.Grupo25.Fase1/PosTech.Contatos.View/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PosTech.Contatos.View.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
