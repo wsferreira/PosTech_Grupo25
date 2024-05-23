f687ca090b4bd7d9449511e4addaedd86ffa193c
Ian <mello.ian@hoymail.com>
2024-05-23 03:22:41 -0300

    ADD TEMPLATE

---
+++ Fase1/PosTech.Grupo25.Fase1/PosTech.Contatos.View/Scripts/Application/Global.js
@@ -0,0 +1,16 @@
+﻿function RequestAjax(url,type,inputData,failMessage,modal) {
+    var result = null;
+    $.ajax({
+        url: url,
+        type: type,
+        async: false,
+        data: inputData,
+        cache: false
+    }).done(function (data) {
+        result = data;
+    }).fail(function (jqXHR, textStatus) {
+        alert(failMessage);
+        $(modal).modal('toggle');
+    });
+    return result;
+}
