5364441c6c011437590eafa5dec068080901dfb3
wsferreira <wferreira.contato@gmail.com>
2024-12-01 20:43:27 -0300

    teste para workflow

--- Fase 4/PosTech.Grupo25.Fase4/k6_test/test.js
+++ Fase 4/PosTech.Grupo25.Fase4/k6_test/test.js
@@ -3,6 +3,7 @@ import { sleep } from 'k6';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
+  //teste
   vus: 150,
   // A string specifying the total duration of the test run.
   duration: '30s',
